cb14af825b401a7bad9638043dac9c9036f12b88
sajjad <ghadimichenar@gmail.com>
2026-06-15 23:29:53 +0330

    Add files via upload

---
+++ 6-حلقه for.py
@@ -0,0 +1,35 @@
+p = ["sajad" , "mmd" , "asghar"]
+
+for x in p:
+    print(x)
+    #خروجی:
+    #sajad
+    #mmd
+    #asghar
+
+    #این یه نوع حلقه است مثل حلقه وایل
+    #این حلقه جزیات داخل متغیر رو برامون پیدا میکنه و نمایش میده
+
+    v = "sj"
+
+for i in v:
+    print(i)
+
+    #خروجیش:
+    #s
+    #j
+
+
+    #ما از len برای حلقه for هم میتونیم استفاده کنیم
+
+b = ["abc" , "abcd" , "abcdf"]
+
+for j in b:
+    print(j, len(j))
+    #خروجیش:
+    #abc 3
+    #abcd 4
+    #abcdf 5
+
+#حلقه فور مخصوص str ها خستن نه int
+
